c22120990c68772d25908c6a98197569dbd9aa59
ahmeddouss <118319834+ahmeddouss@users.noreply.github.com>
2024-05-06 17:33:06 +0200

    Revert "push"
    This reverts commit 75abc4e7cc1040304071f6a70df2ccaf25f12234.

--- src/main/java/com/example/gestionconference/Test/SessionMain/test.java
+++ src/main/java/com/example/gestionconference/Test/SessionMain/test.java
@@ -1,30 +1,44 @@
 package com.example.gestionconference.Test.SessionMain;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
-
-import java.io.IOException;
 
 public class test extends Application {
 
-
     @Override
-    public void start(Stage primaryStage) throws IOException {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/example/gestionconference/Fxml/PresenceFXML/CardManagement.fxml"));
-        Parent root = loader.load();
-        Scene scene = new Scene(root);
-        primaryStage.setTitle("Confera");
-        primaryStage.setScene(scene);
-        primaryStage.show();
-        primaryStage.getScene().getStylesheets().add("com/example/gestionconference/Styles/StyleSheet.css");
+    public void start(Stage primaryStage) {
+        TabPane tabPane = new TabPane();
 
+        Tab tab1 = new Tab("Tab 1");
+        tab1.setContent(new StackPane());
+        Tab tab2 = new Tab("Tab 2");
+        tab2.setContent(new StackPane());
+        Tab tab3 = new Tab("Tab 3");
+        tab3.setContent(new StackPane());
+
+        tabPane.getTabs().addAll(tab1, tab2, tab3);
+
+        tabPane.setStyle("-fx-background-color: #f0f0f0; -fx-background-radius: 10px;"); // Set the background color and radius
+
+        for (Tab tab : tabPane.getTabs()) {
+            tab.setStyle("-fx-background-color: #d0d0d0; -fx-border-color: #a0a0a0; -fx-border-width: 1px; -fx-border-style: solid; -fx-padding: 5 10;");
+        }
+
+        tabPane.getTabs().get(0).setStyle(tabPane.getTabs().get(0).getStyle() + "-fx-background-radius: 20px 0 0 0;"); // Apply border radius to the first tab
+
+        Scene scene = new Scene(tabPane, 400, 300);
+        scene.getStylesheets().add("/com/example/gestionconference/Styles/StyleSheet.css");
+
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Styled TabPane Example");
+        primaryStage.show();
     }
 
     public static void main(String[] args) {
         launch(args);
     }
-
 }
